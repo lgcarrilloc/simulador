99c23d944089cfecd5171d95f5cb6e3e79e1bb35
lgcarrilloc <mgoluiswar@hotmail.com>
2026-04-16 18:25:40 -0500

    Se agregaron demas funciones

--- funciones.js
+++ funciones.js
@@ -6,14 +6,33 @@ function calcularDisponible(ingresos, egresos){
     }
     return disponible;
 }
- 
 function calcularCapacidadDePago(montoDisponible){
     let capasidadDePago = montoDisponible / 2;
  
     return capasidadDePago;
 }
- 
 function texto (id,vari){
     let texto1 = document.getElementById(id);
     texto1.innerText=vari.toFixed(2);    
+}
+function calcularInteresSimple(monto, tasa, plazoAños){
+    let interes = monto * (tasa/100) * plazoAños;
+    return interes;
+}
+function calcularTotalPagar(monto, interes){
+    let total = monto + interes + 100; // 100 contribucion solca
+    return total;
+}
+function calcularCuotaMensual(total, plazoAños){
+    let meses = plazoAños * 12;
+    let cuotaMensual = total / meses;
+    return cuotaMensual;
+}
+
+function aprobarCredito(capacidadPago, cuotaMensual){
+    if(capacidadPago > cuotaMensual){
+        return true;
+    } else {
+        return false;
+    }
 }
--- simulador.js
+++ simulador.js
@@ -3,6 +3,26 @@ function calcular() {
     let egresos = parseFloat(document.getElementById("txtEgresos").value);
     let disponible = calcularDisponible(ingresos, egresos);
     texto("spnDisponible", disponible);
+    
     let capasidadDePago = calcularCapacidadDePago(disponible);
     texto("spnCapacidadPago", capasidadDePago);
+    
+    let monto = parseInt(document.getElementById("txtMonto").value);
+    let plazoAños = parseInt(document.getElementById("txtPlazo").value);
+    let tasa = parseInt(document.getElementById("txtTasaInteres").value);
+    let interes = calcularInteresSimple(monto, plazoAños, tasa);
+    texto("spnInteresPagar", interes);
+
+    let totalPagar = calcularTotalPagar(monto, interes);
+    texto("spnTotalPrestamo", totalPagar);
+
+    let cuotaMensual = calcularCuotaMensual(totalPagar, plazoAños);
+    texto("spnCuotaMensual", cuotaMensual);
+
+    let aprobado = aprobarCredito(capasidadDePago, cuotaMensual);
+    if (aprobado){
+        document.getElementById("spnEstadoCredito").innerText="CREDITO APROBADO";
+        } else {
+        document.getElementById("spnEstadoCredito").innerText="CREDITO RECHAZADO";
+        }
 }
