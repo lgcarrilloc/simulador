f1add21e1169fcf5350056b6a65d7747956c06c7
lgcarrilloc <mgoluiswar@hotmail.com>
2026-04-16 16:18:09 -0500

    primeros cambios simulador

--- funciones.js
+++ funciones.js
@@ -1,1 +1,19 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible(ingresos, egresos){
+    let disponible = ingresos - egresos;
+    if(disponible < 0){
+        disponible = 0;
+    }
+    return disponible;
+}
+ 
+function calcularCapacidadDePago(montoDisponible){
+    let capasidadDePago = montoDisponible / 2;
+ 
+    return capasidadDePago;
+}
+ 
+function texto (id,vari){
+    let texto1 = document.getElementById(id);
+    texto1.innerText=vari.toFixed(2);    
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,8 @@
-//AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular() {
+    let ingresos = parseFloat(document.getElementById("txtIngresos").value);
+    let egresos = parseFloat(document.getElementById("txtEgresos").value);
+    let disponible = calcularDisponible(ingresos, egresos);
+    texto("spnDisponible", disponible);
+    let capasidadDePago = calcularCapacidadDePago(disponible);
+    texto("spnCapacidadPago", capasidadDePago);
+}
